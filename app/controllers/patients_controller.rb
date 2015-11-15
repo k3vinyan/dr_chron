a057f47d2d18235db663180a82a5ffbd2b1bfa9b
Chris Scott <christo247@gmail.com>
2015-11-15 12:51:08 -0800

    basic post functionality good to go

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,9 +3,10 @@ class PatientsController < ApplicationController
     response = HTTParty.post('https://drchrono.com/api/patients',
       :body => {
         :first_name => params["first_name"],
+        :middle_name => params["middle_name"],
         :last_name => params["last_name"],
         :gender => params["gender"],
-        :date_of_birth => params["date_of_birth"],
+        :date_of_birth => format_date(params["date"]),
         :doctor => current_user.doctor_id
       }.to_json,
       :headers => {
@@ -15,4 +16,9 @@ class PatientsController < ApplicationController
 
     fail
   end
+
+  private
+    def format_date date
+      "#{date['year']}-#{date['month']}-#{date['day']}"
+    end
 end
