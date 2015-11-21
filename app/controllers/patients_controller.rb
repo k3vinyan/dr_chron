5e7f8cef6c0769519fae73cce059443721e09059
Chris Scott <christo247@gmail.com>
2015-11-21 09:11:00 -0800

    abstract out patient params, update patients now working

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,25 +1,10 @@
 class PatientsController < ApplicationController
   def create
     response = HTTParty.post("https://drchrono.com/api/patients",
-      :body => {
-        :first_name => params["first_name"],
-        :last_name => params["last_name"],
-        :gender => params["gender"],
-        :date_of_birth => format_date(params["date"]),
-        :doctor => current_user.doctor_id,
-        :address => params["address"],
-        :city => params["city"],
-        :state => params["state"],
-        :zip_code => params["zip_code"],
-        :home_phone => params["home_phone"],
-        :cell_phone => params["cell_phone"],
-        :emergency_contact_name => params["emergency_contact_name"],
-        :emergency_contact_phone => params["emergency_contact_phone"],
-        :emergency_contact_relation => params["emergency_contact_relation"],
-      }.to_json,
-        :headers => {
-          "Content-Type" => "application/json",
-          "Authorization" => "Bearer #{current_user.access_token}",
+      :body => patient_params,
+      :headers => {
+        "Content-Type" => "application/json",
+        "Authorization" => "Bearer #{current_user.access_token}",
     })
 
     redirect_to patients_path
@@ -35,20 +20,10 @@ class PatientsController < ApplicationController
 
   def update
     response = HTTParty.patch("https://drchrono.com/api/patients/#{params['id']}",
-      :body => {
-        :first_name => params["first_name"],
-        :last_name => params["last_name"],
-        :gender => params["gender"],
-        :date_of_birth => format_date(params["date"]),
-        :doctor => current_user.doctor_id,
-        :address => params["address"],
-        :city => params["city"],
-        :state => params["state"],
-        :copay => params["copay"]
-      }.to_json,
-        :headers => {
-          "Content-Type" => "application/json",
-          "Authorization" => "Bearer #{current_user.access_token}",
+      :body => patient_params,
+      :headers => {
+        "Content-Type" => "application/json",
+        "Authorization" => "Bearer #{current_user.access_token}",
     })
 
     redirect_to patient_path(params["id"])
@@ -57,5 +32,29 @@ class PatientsController < ApplicationController
   private
     def format_date date
       "#{date['year']}-#{date['month']}-#{date['day']}"
+    end
+
+    def patient_params
+      {
+        :first_name => params["first_name"],
+        :last_name => params["last_name"],
+        :gender => params["gender"],
+        :date_of_birth => format_date(params["date"]),
+        :doctor => current_user.doctor_id,
+        :address => params["address"],
+        :city => params["city"],
+        :state => params["state"],
+        :zip_code => params["zip_code"],
+        :home_phone => params["home_phone"],
+        :cell_phone => params["cell_phone"],
+        :emergency_contact_name => params["emergency_contact_name"],
+        :emergency_contact_phone => params["emergency_contact_phone"],
+        :emergency_contact_relation => params["emergency_contact_relation"],
+        :employer => params["employer"],
+        :employer_address => params["employer_address"],
+        :employer_city => params["employer_city"],
+        :employer_state => params["employer_state"],
+        :employer_zip_code => params["employer_zip_code"],
+      }.to_json
     end
 end
